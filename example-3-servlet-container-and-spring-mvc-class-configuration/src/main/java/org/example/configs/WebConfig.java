827b70efb73bc538a25d3c5d6dba20392866f0c6
kjeff24 <jeffreyarthur123@gmail.com>
2024-12-03 11:24:20 +0000

    Added class based configuration

---
+++ example-3-servlet-container-and-spring-mvc-class-configuration/src/main/java/org/example/configs/WebConfig.java
@@ -0,0 +1,23 @@
+package org.example.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("org.example.controllers")
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    // Define the InternalResourceViewResolver bean to resolve view names to .jsp files.
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
